01234e7c772453a788eca4dcbdc765bceff61a6a
phop <151123064+phop01@users.noreply.github.com>
2025-10-11 16:44:18 +0700

    add all team keys

--- src/types/TeamKeys.ts
+++ src/types/TeamKeys.ts
@@ -1,16 +1,25 @@
-import Manchester_United_FC_logo from '../assets/icons/Manchester_United_FC_logo.svg'
-import Manchester_City_2016 from '../assets/icons/Manchester_City_2016.svg'
 import Arsenal_FC_logo from '../assets/icons/Arsenal_FC_logo.svg'
 import Aston_Villa_FC_logo from '../assets/icons/Aston_Villa_FC_2015.webp'
 import brentford_fc from '../assets/icons/Brentford_FC_2017.webp'
 import  brighton_hove_albion_fc from '../assets/icons/Brighton_&_Hove_Albion_FC_logo.svg'
 import chelsea_fc from '../assets/icons/Chelsea_FC_logo.svg'
-import Crystal_palaxe_fc from '../assets/icons/Crystal_Palace_FC_logo_(introduced_2013).svg'
-import ecerton_fc from '../assets/icons/Everton_FC_logo_(introduced_2014).svg'
+import Crystal_Palace from '../assets/icons/Crystal_Palace_FC_logo_(introduced_2013).svg'
+import Everton from '../assets/icons/Everton_FC_logo_(introduced_2014).svg'
 import fulham_fc from '../assets/icons/Fulham_FC_logo.svg'
 import Leeds_United_fc from '../assets/icons/Leeds_United_AFC_logo.svg'
 import leicester_city_fc from '../assets/icons/Leicester_City_FC_logo.webp'
 import Liverpool_fc from '../assets/icons/Liverpool_FC_logo.svg'
+import Manchester_City_2016 from '../assets/icons/Manchester_City_2016.svg'
+import Manchester_United_FC_logo from '../assets/icons/Manchester_United_FC_logo.svg'
+import Newcastle from '../assets/icons/Newcastle_United_FC_logo.svg'
+import Nottingham_Forest from '../assets/icons/Nottingham_Forest_FC_logo_2C_two_stars_below%29.webp'
+import Southampton_fc from '../assets/icons/Southampton_FC_logo.svg'
+import Tottenham_Hotspur_fc from '../assets/icons/Tottenham_Hotspur_FC_logo.svg'
+import West_Ham_United_fc from '../assets/icons/West_Ham_United_FC_logo_(2016).svg'
+import Wolverhampton_Wanderers_fc from '../assets/icons/Wolverhampton_Wanderers_FC_logo.svg'
+import Burnley from '../assets/icons/Burnley_FC_Logo.svg.png'
+import Sunderland from '../assets/icons/Logo_Sunderland.svg.png'
+import Bournemouth from '../assets/icons/o3e59plsgm0Gtt43tpi-o.png'
 
 
 
@@ -18,7 +27,25 @@ const TeamKeys = {
     man_utd: Manchester_United_FC_logo,
     man_city: Manchester_City_2016,
     arsenal: Arsenal_FC_logo,
-    aston_villa: Aston_Villa_FC_logo
+    aston_villa: Aston_Villa_FC_logo,
+    brentford: brentford_fc,
+    brighton: brighton_hove_albion_fc,
+    chelsea: chelsea_fc,
+    crystal_palaxe: Crystal_Palace,
+    everton: Everton,
+    fulham: fulham_fc,
+    leeds: Leeds_United_fc,
+    leicester: leicester_city_fc,
+    liverpool: Liverpool_fc,
+    newcastle: Newcastle,
+    nottingham_forest: Nottingham_Forest,
+    southampton: Southampton_fc,
+    tottenham: Tottenham_Hotspur_fc,
+    west_ham: West_Ham_United_fc,
+    wolves: Wolverhampton_Wanderers_fc,
+    burnley: Burnley,
+    sunderland: Sunderland,
+    bournemouth: Bournemouth
 
 
 
